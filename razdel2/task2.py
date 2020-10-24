c9424006aa465ff69909ab38f6acb45dc0327ea7
Павел Гавриличев <PNGavrilichev@stud.kpfu.ru>
2020-10-24 13:38:30 +0300

    almost task2

--- razdel2/task2.py
+++ razdel2/task2.py
@@ -7,9 +7,13 @@ import math
 alf = 0.05
 l = 1.5      # Лямбда для показательного распределения
 
-
+#функция показательного распределения
 def eps_fr(l,x):
     return (1 - math.exp(-l*x))
+
+def kolm_quantil():
+    return math.sqrt((-1/2)*(math.log(alf/2)))
+
 
 def graph_EFR(sample):
     distribution = sorted(collections.Counter(sample).most_common(), key=lambda elem: elem[0])
@@ -19,11 +23,11 @@ def graph_EFR(sample):
     sum_for_y = 0
     for pair in distribution:
         if pair != distribution[0]:
-            x.append(previous[0])
+            x.append(prev[0])
             y.append(sum_for_y/len(sample))
         x.append(pair[0])
         y.append(sum_for_y/len(sample))
-        previous = pair
+        prev = pair
         sum_for_y += pair[1]
 
     plt.plot(x, y)
@@ -36,4 +40,11 @@ def graph_EFR(sample):
     plt.ylabel('E(X)')
     plt.show()
 
-graph_EFR(sample)
+#graph_EFR(sample)
+
+D_n = max([math.fabs(value - eps_fr(l,value)) for value in sample])
+statistic = D_n * math.sqrt(len(sample))
+k_quantil = kolm_quantil()
+
+print(statistic, k_quantil)
+
