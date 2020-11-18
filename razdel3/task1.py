10a23a0afd021214f56a36cdd44c69157c5f59c4
Павел Гавриличев <PNGavrilichev@stud.kpfu.ru>
2020-11-18 18:49:10 +0300

    upd

--- razdel3/task1.py
+++ razdel3/task1.py
@@ -24,7 +24,11 @@ k = sum(x1)
 n = len(x1)
 print("Логарифм фукнции правдоподобия: ln L = ln C(n,k) + k * ln p + (n-k) * ln(1-p)")
 print("Оценка для параметра p: k/n")
-print(k/n)
+print(f"Вычисленное значение p для данной выборки при помощи полученной оценки: {k/n}")
 p1 = 0.4
 p2 = 0.5
 p3 = 0.6
+
+# print(binom_P(k,n,p1))
+# print(binom_P(k,n,p2))
+# print(binom_P(k,n,p3))
